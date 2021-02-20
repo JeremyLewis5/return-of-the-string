d3f6320eadd69ffb9dae15ebc74cbdf11295f883
JeremyLewis5 <jeremy.lewis@codeimmersives.com>
2021-02-20 15:08:48 -0600

    changes

--- main.js
+++ main.js
@@ -2,6 +2,51 @@
  * YOUR CODE HERE! *
  *******************/
 
+ function yell(str) {
+      return str + "!";
+ }
+
+ 
+ //let result = 'Colin'[0];
+ //result;
+ //let soda = 'Coke';
+ //let lastChar = soda[soda.length - 1];
+ //lastChar;
+ //let index = soda.indexOf('o');
+ //index;
+ 
+ function getFirstCharacter(str) {
+        return str[0];
+ }
+
+ function getLastCharacter(last) {
+        return last[last.length - 1];
+ }
+ 
+ function getOneCharacter(str, number1) {
+        return  str[0];
+ }//Need to review
+
+ function getTwoCharacter(str, number2, number3) {
+        return str[str.length - 1];
+ }//Need to review
+
+ function makeCapitalized(str) {
+        return str.toUpperCase(str);
+ }
+
+ function yellLouder(str) {
+        return str.toUpperCase(str) + '!!!';
+
+ }
+
+ function getInitials(string) {
+        let str = str.indexOf([0])
+
+
+ }//Need to review
+
+ 
 
 
 
